c6f0e269214f036fd4a81bfd3d2580c578f208da
laiscrz <lais.albino2003@gmail.com>
2023-10-04 19:39:44 -0300

    Add tratamento de 'game over' no JS

--- JS/script.js
+++ JS/script.js
@@ -21,6 +21,15 @@ const loop = setInterval(()=>{
         pipe.style.animation = 'none';
         pipe.style.left = `${pipePosition}px`;
 
+        mario.style.animation = 'none';
+        mario.style.bottom = `${marioPosition}px`;
+
+        mario.src = './images/game-over.png'
+        mario.style.width = '75px'
+        mario.style.marginLeft = '50px'
+
+
+        clearInterval(loop);
     }
 
 },10)
